0007035e1d49bf5718212bed02b894d5301ffa9f
Turbofreighter <sudhb38@gmail.com>
2023-10-20 20:27:38 -0500

    test

---
+++ src/components/Header.svelte
@@ -0,0 +1,43 @@
+<header>
+  <div
+    class="w-full h-[4.5em] flex-row items-center inline-flex bg-violet-100 gap-2.5"
+  >
+    <div
+      class="left-[20px] w-[4em] h-[4em] bg-zinc-400 rounded-[10px] shadow border m-3"
+    />
+    <div class="flex-auto" />
+    <div
+      class="right-[0px] px-[6em] py-[1em] justify-end items-start gap-[4.5em] flex"
+    >
+      <div
+        class="text-right text-black text-xl font-normal font-['Nunito Sans']"
+      >
+        <button type="button"> PROJECTS </button>
+      </div>
+      <div
+        class="text-right text-black text-xl font-normal font-['Nunito Sans']"
+      >
+        <button type="button">RESOURCES</button>
+      </div>
+      <div
+        class="text-right text-black text-xl font-normal font-['Nunito Sans']"
+      >
+        <button type="button">CONTACT US</button>
+      </div>
+      <div
+        class="text-right text-black text-xl font-normal font-['Nunito Sans']"
+      >
+        <button type="button">ABOUT US</button>
+      </div>
+    </div>
+  </div>
+</header>
+
+<style>
+  @media only screen and (max-width: 768px) {
+    /* Styles for screens smaller than 768px */
+    .w-full {
+      width: 100%;
+    }
+  }
+</style>
