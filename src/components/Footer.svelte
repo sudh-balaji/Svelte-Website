94cdaad69cb4b1f181fd6abff469b14302d5db60
Turbofreighter <sudhb38@gmail.com>
2023-11-27 18:18:49 -0600

    Added footer and main page template

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+  import InstaIcon from "./assets/Instagram.png";
+  import DiscIcon from "./assets/Discord.png";
+</script>
+
+<div class="bg-indigo-800 py-4">
+  <div class="container mx-auto px-4 content-center space-y-4">
+    <p class="text-center text-white">How to Reach Us</p>
+    <div class="w-max flex mx-auto space-x-20">
+      <img src={DiscIcon} alt="Discord Icon" class="mx-auto h-20" />
+      <img src={InstaIcon} alt="Instagram Icon" class="mx-auto h-20" />
+    </div>
+    <p class="text-center text-white">
+      Membership Form +
+      <a
+        href="https://discord.gg/EzhkDvvWk5"
+        target="_blank"
+        rel="noopener noreferrer">Join our Discord</a
+      >
+    </p>
+  </div>
+  <div class="container mx-auto px-4"></div>
+</div>
